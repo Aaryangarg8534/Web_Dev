e6212d6d22f7e96cd6b3d7efe245e5888f4a4654
Aaryan garg <ajayaaryan4u@gmail.com>
2021-09-18 18:08:49 +0530

    Intermediate of Javascript

--- Aaryan/Javascript/Simon Game Project/index.js
+++ Aaryan/Javascript/Simon Game Project/index.js
@@ -30,13 +30,21 @@ $(".btn").click(function() {
 function checkAnswer(currentLevel){
     if(gamePattern[currentLevel] ===userClickedPattern[currentLevel] ){
         if(userClickedPattern.length === gamePattern.length){
-            console.log("sucess");
             setTimeout(function (){
                 nextSequence();
             },1000);
         }
     }else{
-        console.log("wrong");
+
+        playSound("wrong");
+        
+        $("body").addClass("game-over");
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
     }
 }
 
