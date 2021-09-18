fe21d94b3f6a93d68244bd890a1b0cc13bbde447
Aaryan garg <ajayaaryan4u@gmail.com>
2021-09-18 18:13:20 +0530

    Intermediate of Javascript

--- Aaryan/Javascript/Simon Game Project/index.js
+++ Aaryan/Javascript/Simon Game Project/index.js
@@ -13,15 +13,15 @@ $(document).keypress(function() {
       nextSequence();
       started = true;
     }
-  });
+});
 
 $(".btn").click(function() {
 
     var userChosenColour = $(this).attr("id");
-  
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
+
     animatePress(userChosenColour);
     
     checkAnswer(userClickedPattern.length-1);
@@ -29,6 +29,7 @@ $(".btn").click(function() {
 
 function checkAnswer(currentLevel){
     if(gamePattern[currentLevel] ===userClickedPattern[currentLevel] ){
+
         if(userClickedPattern.length === gamePattern.length){
             setTimeout(function (){
                 nextSequence();
@@ -37,7 +38,7 @@ function checkAnswer(currentLevel){
     }else{
 
         playSound("wrong");
-        
+
         $("body").addClass("game-over");
 
         $("#level-title").text("Game Over, Press Any Key to Restart");
@@ -45,17 +46,22 @@ function checkAnswer(currentLevel){
         setTimeout(function(){
             $("body").removeClass("game-over");
         },200);
+
+        startOver();
     }
 }
 
 function nextSequence() {
     userClickedPattern = [];
+
     level++;
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100);
+
     playSound(randomChosenColour);
 }
 
@@ -66,7 +72,14 @@ function playSound(name) {
 
 function animatePress(currentColour){
    $("#"+currentColour).addClass("pressed");
+
     setTimeout (function (){
         $("#"+currentColour).removeClass("pressed");
-    },100);
+    }, 100);
+}
+
+function startOver(){
+    level=0;
+    started = false;
+    gamePattern =[];  
 }
