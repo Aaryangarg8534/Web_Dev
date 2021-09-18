99e4cb5bb4304c2449078ec101fc83ff4d4b08a8
Aaryan garg <ajayaaryan4u@gmail.com>
2021-09-18 18:21:44 +0530

    Intermediate of Javascript

--- Aaryan/Javascript/Simon Game Project/index.js
+++ Aaryan/Javascript/Simon Game Project/index.js
@@ -4,8 +4,8 @@ var gamePattern = [];//an Empty array was created;
 
 var userClickedPattern=[];
 
-var level = 0;
 var started=false;
+var level = 0;
 
 $(document).keypress(function() {
     if (!started) {
@@ -55,27 +55,27 @@ function nextSequence() {
     userClickedPattern = [];
 
     level++;
-
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     playSound(randomChosenColour);
+}
+
+function animatePress(currentColour){
+   $("#" + currentColour).addClass("pressed");
+
+    setTimeout (function (){
+        $("#" +  currentColour).removeClass("pressed");
+    }, 100);
 }
 
 function playSound(name) {
     var audio = new Audio("sounds/" + name +".mp3");
     audio.play();
-}
-
-function animatePress(currentColour){
-   $("#"+currentColour).addClass("pressed");
-
-    setTimeout (function (){
-        $("#"+currentColour).removeClass("pressed");
-    }, 100);
 }
 
 function startOver(){
