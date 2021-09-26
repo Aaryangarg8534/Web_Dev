78ab7fd56f0c71bdeca82496c1f33d6e28ca4edd
Aaryan garg <ajayaaryan4u@gmail.com>
2021-09-26 14:58:22 +0530

    More into Javascript

--- Aaryan/Javascript/Model Button Project/script.js
+++ Aaryan/Javascript/Model Button Project/script.js
@@ -1,1 +1,26 @@
 'use strict';
+// In this i Learn adding and removing of classes
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClosemodal = document.querySelector('.close-modal');
+const btnOpenmodal = $('.show-modal');
+console.log(btnOpenmodal);
+
+for (let i = 0; i < btnOpenmodal.length; i++) {
+  btnOpenmodal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnClosemodal.addEventListener('click', function () {
+  //console.log('Button clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
