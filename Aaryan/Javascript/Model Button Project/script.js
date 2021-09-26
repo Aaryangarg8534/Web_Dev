99e1e210de70b87039f3099d21021fe03aabdb8a
Aaryan garg <ajayaaryan4u@gmail.com>
2021-09-26 15:12:41 +0530

    Learing More things in JAvascript

--- Aaryan/Javascript/Model Button Project/script.js
+++ Aaryan/Javascript/Model Button Project/script.js
@@ -4,16 +4,28 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnClosemodal = document.querySelector('.close-modal');
 const btnOpenmodal = $('.show-modal');
-console.log(btnOpenmodal);
+//console.log(btnOpenmodal);
+
+const openModel = function () {
+  console.log('Button cliked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnOpenmodal.length; i++) {
-  btnOpenmodal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnOpenmodal[i].addEventListener('click', openModel);
 }
 
+btnClosemodal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+/*I refactoring(Short) my code 
 btnClosemodal.addEventListener('click', function () {
   //console.log('Button clicked');
   modal.classList.add('hidden');
@@ -23,4 +35,4 @@ btnClosemodal.addEventListener('click', function () {
 overlay.addEventListener('click', function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+});*/
